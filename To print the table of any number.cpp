bc4b27bc415f0e6ed838767fd18dce2b0e9d89d2
Muhammad Hussain <93116251+Muhammadussain@users.noreply.github.com>
2022-02-11 00:19:39 +0500

    Add files via upload

---
+++ To print the table of any number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,i;
+	cout<<"enter the value of a"<<endl;
+	cin>>a;
+	for(i=0;i<=100;i++){
+	cout<<a<< " x "<<i<< "=" <<a*i<<endl;
+	}
+	return 0;
+}
